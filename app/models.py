43559a4a7d3aca2a02f5011387a226fc48744c7b
jzc <justincai.lps@gmail.com>
2017-11-03 13:05:52 -0600

    finished data script and models

--- app/models.py
+++ app/models.py
@@ -1,44 +1,46 @@
 from . import db
 
-# Model - User
-# id
-# email
-# first 
-# last
-# screen name - unique
-# password (stored in hash)
-# score/rep
+user_course = db.Table("user_course",
+                       db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+                       db.Column("course_id", db.Integer, db.ForeignKey("courses.id"), primary_key=True))
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128))
-    first = db.Column(db.String(32))
-    last = db.Column(db.String(32))
-    screen_name = db.Column(db.String(20))
+    email = db.Column(db.String)
+    first = db.Column(db.String)
+    last = db.Column(db.String)
+    screen_name = db.Column(db.String)
+    courses = db.relationship("Course", secondary=user_course)
 
-# Model - UserCourse
-# id
-# UserId
-# ClassId
+class Course(db.Model):
+    __tablename__ = "courses"
+    id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.String)
+    course_number = db.Column(db.Integer)
+    course_name = db.Column(db.String)
+    users = db.relationship("User", secondary=user_course)
 
-# Model - Course
-# id
-# department
-# course number
-# course name
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    is_link = db.Column(db.Boolean)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User", backref="posts")
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"))
+    course = db.relationship("Course", backref="posts")
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
 
-# Model - Post
-# id
-# isLink
-# content
-# CourseID 
-# VotableID
-
-# Model - Comment
-# id
-# VotableID
-
-# Model - Votable
-# id
-# upvotes 
-# downvotes
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    post = db.relationship("Post", backref="comments")
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User", backref="comments")
+    content = db.Column(db.String)
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
--- test/create_test_data.py
+++ test/create_test_data.py
@@ -1,13 +1,78 @@
-import random
-from app import db
+import sys
+sys.path.append('..')
 
-# create some users
-words = [line.strip() for line in open("words.txt", "r")]
-n_users = 1000
-for i in range(n_users):
-    first = random.choice(words)
-    last = random.choice(words)
-    
-# create some courses
-# create some posts
-# create some comments
+import random
+
+from app import create_app
+
+with create_app("development").app_context():
+    from app import db
+    from app.models import *
+    db.drop_all()
+    db.create_all()
+    # create some courses
+    courses = []
+
+    with open("course.csv") as f:
+        next(f)
+        for line in f:
+            tokens = line.strip().split(",")
+            data = {
+                "department": tokens[0],
+                "course_number": tokens[1],
+                "course_name": tokens[2],
+            }
+            c = Course(**data)
+            courses.append(c)
+            db.session.add(c)
+
+    # create some users
+    users = []
+    words = [line.strip() for line in open("words.txt", "r")]
+    n_users = 1000
+    for i in range(n_users):
+        first = random.choice(words)
+        last = random.choice(words)
+        screen_name = "%s.%s" % (first, last)
+        data = {
+            "first": first,
+            "last": last,
+            "screen_name": screen_name,
+            "email": "%s@%s.com" % (screen_name, random.choice(words)),
+            "courses": random.sample(courses, random.randint(1, 5)),
+        }
+        u = User(**data)
+        users.append(u)
+        db.session.add(u)
+
+    # create some posts
+    posts = []
+    n_posts = 1000
+    for i in range(n_posts):
+        data = {
+            "is_link": False,
+            "title": ' '.join(random.sample(words, random.randint(1, 50))),
+            "content": ' '.join(random.sample(words, random.randint(10,100))),
+            "upvotes": random.randint(10,100),
+            "downvotes": random.randint(0,20),
+            "user": random.choice(users),
+            "course": random.choice(courses),
+        }
+        p = Post(**data)
+        posts.append(p)
+        db.session.add(p)
+
+    # create some comments
+    n_comments = 1000
+    for i in range(n_comments):
+        data = {
+            "content": ' '.join(random.sample(words, random.randint(1,50))),
+            "upvotes": random.randint(10,100),
+            "downvotes": random.randint(0,20),
+            "post": random.choice(posts),
+            "user": random.choice(users),
+        }
+        c = Comment(**data)
+        db.session.add(c)
+
+    db.session.commit()
