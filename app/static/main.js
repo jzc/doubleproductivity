2b4fb98e40f2cc8d7ff83f899c39e3eea843096d
jzc <justincai.lps@gmail.com>
2017-11-12 20:39:48 -0700

    style changes

--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,12 @@
-document.getElementById("expand").onclick = expand
+document.getElementById("expand").onclick = expand;
+document.getElementById("close").onclick = close;
+
+let navbarWidth = "175px";
 
 function expand() {
-    document.getElementById("sidebar").style.width="0%";
-    document.getElementById("content").style.marginLeft="0%";
+    document.getElementById("main").style.marginLeft=navbarWidth;
+}
+
+function close() {
+    document.getElementById("main").style.marginLeft="0%";
 }
