de10779883d83833a46813fcd4e29172ab23d367
jzc <justincai.lps@gmail.com>
2017-11-12 17:16:36 -0700

    some navbar stuff

---
+++ app/static/main.js
@@ -0,0 +1,6 @@
+document.getElementById("expand").onclick = expand
+
+function expand() {
+    document.getElementById("sidebar").style.width="0%";
+    document.getElementById("content").style.marginLeft="0%";
+}
