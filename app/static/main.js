05ea24dfdd408e685bc4acdc1de96fb2d21e14a5
jzc <justincai.lps@gmail.com>
2017-11-13 15:06:29 -0700

    finished sidebar animation

--- app/static/main.js
+++ app/static/main.js
@@ -5,8 +5,10 @@ let navbarWidth = "175px";
 
 function expand() {
     document.getElementById("main").style.marginLeft=navbarWidth;
+    document.getElementById("main").style.width = "calc(100% - 175px)";
 }
 
 function close() {
     document.getElementById("main").style.marginLeft="0%";
+    document.getElementById("main").style.width="100%";
 }
