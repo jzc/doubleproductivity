7811e24abb4d602a2d2f5c1495c7ccbc1fd43fa0
jzc <justincai.lps@gmail.com>
2017-12-11 19:49:41 -0700

    hamburger

--- app/static/main.js
+++ app/static/main.js
@@ -1,22 +1,35 @@
-document.getElementById("expand").onclick = expand;
-document.getElementById("close").onclick = close;
+// document.getElementById("expand").onclick = expand;
+// document.getElementById("close").onclick = close;
 
 let navbarWidth = "175px";
 
-function expand() {
-    document.getElementById("main").style.marginLeft=navbarWidth;
-    document.getElementById("main").style.width = "calc(100% - 175px)";
-}
+// function expand() {
+//     document.getElementById("main").style.marginLeft=navbarWidth;
+//     document.getElementById("main").style.width = "calc(100% - 175px)";
+// }
 
-function close() {
-    document.getElementById("main").style.marginLeft="0%";
-    document.getElementById("main").style.width="100%";
-}
+// function close() {
+//     document.getElementById("main").style.marginLeft="0%";
+//     document.getElementById("main").style.width="100%";
+// }
 
 flash_list = document.getElementById("flashes").children[0].children;
 for (let i = 0; i < flash_list.length; i++)
 {
     flash_list[i].children[0].onclick = function() {
         this.parentElement.remove()
+    }
+}
+
+document.getElementsByClassName("hamburger")[0].onclick = function() {
+    if (document.getElementById("main").style.width=="100%") 
+    {
+        document.getElementById("main").style.marginLeft=navbarWidth;
+        document.getElementById("main").style.width = "calc(100% - 175px)";
+    }
+    else
+    {
+        document.getElementById("main").style.marginLeft="0%";
+        document.getElementById("main").style.width="100%";
     }
 }
