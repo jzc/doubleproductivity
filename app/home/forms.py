58737632acf685dc0dd5dc4a1fe5bd5fed84b672
Matt CU <mame6897@gmail.com>
2017-12-12 17:10:16 -0700

    added basic logic for create post

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import Required
-from wtforms import StringField, TextAreaField, RadioField, SelectField, SubmitField
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
 
 class PostForm(FlaskForm):
 	title = StringField("Title", validators=[Required()])
 	content = TextAreaField("Content", validators=[Required()])
-	is_link = RadioField("Post Type", choices=[(1, "Link Post"),(0, "Self Post")], validators=[Required()], coerce=int)
+	is_link = BooleanField("Link Post")
 	course = SelectField("Class", choices=[(1, "1300"),(2,"3104"),(3,"2824")], validators=[Required()], coerce=int)
 	category = SelectField("Category", choices=[(1,"Tutoring"),(2,"Question"),(3,"Bug Help")], validators=[Required()], coerce=int)
 	submit = SubmitField("Post")
+
+
 	
--- app/home/views.py
+++ app/home/views.py
@@ -2,17 +2,33 @@ from flask import render_template
 from .forms import PostForm
 from . import home
 from ..models import User, Post
+from flask_login import current_user, login_required
 
 @home.route("/")
 def homeIndex():
     return render_template("index.html", posts=Post.query.all()[:10])
     
 @home.route("/create_post", methods=["POST","GET"])
+@login_required
 def createPost():
     print("hello")
     form = PostForm()
     if form.validate_on_submit():
         # logic for putting post in database
+        if (form.is_link):
+            pass
+            # check if content is a url
+            
+        new_post = Post(is_link=form.is_link.data, 
+                                title=form.title.data,
+                                content=form.content.data, 
+                                user=current_user,
+                                #needs course info
+                                upvotes=0,
+                                downvotes=0)
+        db.session.add(new_post)
+        db.session.commit()
+
         raise Exception
         pass
     return render_template("create_post.html", form=form)
@@ -23,3 +39,4 @@ def createPost():
 #         return "Hello, %s" % screen_name
 #     else: 
 #         return "User not found"
+
